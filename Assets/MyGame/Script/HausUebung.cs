96dffb6fa024fde58258d6924bfe36e32bc56176
Janine Sallmann <janine.sallmann@htl-salzburg.ac.at>
2023-01-18 08:47:21 +0100

    Update Hausübung

--- Assets/MyGame/Script/HausUebung.cs
+++ Assets/MyGame/Script/HausUebung.cs
@@ -12,6 +12,16 @@ public class HausUebung : MonoBehaviour
 
     private int alphabetIndex = 0;
 
+    public string[,] array = new string[3, 4]
+    {
+        {"A", "B", "C", "D" },
+        {"E", "F", "G", "H" },
+        {"I", "J", "K", "L" }
+    };
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +36,21 @@ public class HausUebung : MonoBehaviour
             }
         }
 
+        array[0, 0] = "A";
+        array[0, 1] = "B";
+        array[0, 2] = "C";
+        array[0, 3] = "D";
+        array[1, 0] = "E";
+        array[1, 1] = "F";
+        array[1, 2] = "G";
+        array[1, 3] = "H";
+        array[2, 0] = "I";
+        array[2, 1] = "J";
+        array[2, 2] = "K";
+        array[2, 3] = "L";
+
+
     }
 
-    
+
 }
